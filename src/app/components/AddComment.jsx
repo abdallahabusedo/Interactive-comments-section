f118db1e9e3ef0d0e655c78b61bfed52dbe87cec
abdallahabusedo <abdallahsedo@hotmail.com>
2023-05-30 01:07:35 +0300

    add comment

--- src/app/components/AddComment.jsx
+++ src/app/components/AddComment.jsx
@@ -6,10 +6,43 @@ import {
   sendButton,
 } from "@/styles/commentsStyle";
 import { Box, Button, TextField } from "@mui/material";
+import axios from "axios";
 import Image from "next/image";
 import React from "react";
+import { formatDateAgo } from "../utils/generals";
 
 const AddComment = ({ userData }) => {
+  const [newComment, setNewComment] = React.useState("");
+  const handleSend = () => {
+    let dataToSend = {
+      content: newComment,
+      createdAt: formatDateAgo(new Date()),
+      score: 0,
+      user: {
+        username: userData.username,
+        image: {
+          png: userData.image.png,
+          webp: userData.image.webp,
+        },
+      },
+      replies: [],
+    };
+    axios
+      .request({
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "http://localhost:3000/comments",
+
+        data: dataToSend,
+      })
+      .then((response) => {
+        location.reload();
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <Box sx={addCommentCard}>
       <Box sx={addCommentContainer}>
@@ -20,8 +53,14 @@ const AddComment = ({ userData }) => {
           alt="pic"
           loading="lazy"
         />
-        <TextField placeholder="Add a comment" sx={{ flex: "1" }} />
-        <Button sx={sendButton}>SEND</Button>
+        <TextField
+          onChange={(e) => setNewComment(e.target.value)}
+          placeholder="Add a comment"
+          sx={{ flex: "1" }}
+        />
+        <Button onClick={handleSend} sx={sendButton}>
+          SEND
+        </Button>
       </Box>
     </Box>
   );
--- src/app/utils/generals.js
+++ src/app/utils/generals.js
@@ -17,3 +17,24 @@ export function getTimeFromString(timeString) {
 
   return 0; // Default case if timeString does not match any format
 }
+export function formatDateAgo(date) {
+  const currentDate = new Date();
+  const timeDiff = currentDate.getTime() - date.getTime();
+  const seconds = Math.floor(timeDiff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+
+  if (months > 0) {
+    return `${months} month${months > 1 ? "s" : ""} ago`;
+  } else if (days > 0) {
+    return `${days} day${days > 1 ? "s" : ""} ago`;
+  } else if (hours > 0) {
+    return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+  } else {
+    return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+  }
+}
